4efb300fe745f426e03578f06f71e306f8ec97bd
Luis Telemaco <ltelemaco14@gmail.com>
2022-01-03 19:54:26 -0400

    Fixing yet more problems

--- src/containers/App.js
+++ src/containers/App.js
@@ -50,7 +50,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("http://localhost:3001")
+    fetch("https://sleepy-island-60743.herokuapp.com")
       .then((response) => response.json())
       .then(console.log());
   }
@@ -87,7 +87,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://sleepy-island-60743.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -97,7 +97,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://sleepy-island-60743.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
